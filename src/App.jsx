bbf9c5e4aeb33a5b638aef112c770d0a78f61ba4
shiracohen1 <shira050104@gmail.com>
2024-01-17 18:18:21 +0200

    not fucking working

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import background1 from './assets/background1' 
-import background2 from './assets/background2'
-import background3 from './assets/background3'
-import background4 from './assets/background4'
-
+import { useEffect, useState } from 'react'
+import Background1 from './assets/background1.png' 
+import Background2 from './assets/background2.png'
+import Background3 from './assets/background3.png'
+import Background4 from './assets/background4.png'
 import './App.css'
 
 function App() {
-  
+  const [currentBackground, setCurrentBackground] = useState("");
+  const [currentNumber, setCurrentNumber] = useState(0);
 
+  useEffect(() => {
+   setCurrentNumber(Math.floor(Math.random() * (4 - 1) + 1));
+   choosingBackground();
+  }, []);
+
+  function choosingBackground() {
+    setCurrentBackground(`Background${currentNumber}`);
+    console.log(currentBackground);
+  }
+  
   return (
-    <>
+    <div style={{ backgroundImage: `url(${Background1})` }} className='body'>
       
-    </>
+    </div>
   )
 }
 
