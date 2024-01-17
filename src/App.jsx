d33ce28561041340cbd9e330f04070a206a2e72d
shiracohen1 <shira050104@gmail.com>
2024-01-17 16:57:14 +0200

    first commit:

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import background1 from './assets/background1' 
+import background2 from './assets/background2'
+import background3 from './assets/background3'
+import background4 from './assets/background4'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
