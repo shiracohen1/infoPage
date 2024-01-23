846c7aabd12ef084b3f798d42c9f2316c7539c7d
shiracohen1 <shira050104@gmail.com>
2024-01-23 08:54:29 +0200

    preserve aspect ratio and lunch

--- src/foodCourt.jsx
+++ src/foodCourt.jsx
@@ -1,5 +1,9 @@
 import FoodIcon from './assets/foodIcon.svg'
 import LunchRoom from './assets/lunchRoom.svg'
+import OpenHours from './assets/openHours.svg'
+import Kosher from './assets/kosher.svg'
+import Kashrut from './assets/kashrut.svg'
+import MiddleMeal from './assets/middleMeal.svg'
 import './foodCourt.css'
 
 const FoodCourt = () => {
@@ -12,17 +16,74 @@ const FoodCourt = () => {
             <div className='genericContainer'>
                 <img src={LunchRoom} alt="room" className='lunchPic' />
                 <div className='genericText'>
-                בקריית ההדרכה ישנם 6 חדרי אוכל.
-    פעילים A, B, C, D אוכל חדרי
-    במהלך כל השבוע ובכל השעות
-    הנקובות.
-    חדר אוכל E הוא פיתייה ופעיל בימים
-    א׳-ה׳ במהלך שעות ארוחת הצהריים
-    בלבד.
+                    בקריית ההדרכה ישנם 6 חדרי אוכל.
+                    פעילים A, B, C, D אוכל חדרי
+                    במהלך כל השבוע ובכל השעות
+                    הנקובות.
+                    חדר אוכל E הוא פיתייה ופעיל בימים
+                    א׳-ה׳ במהלך שעות ארוחת הצהריים
+                    בלבד.
                 </div>
                 <div className='foodText'>
                     <div className='openingContainer'>
-
+                        <img src={OpenHours} alt="openHours" className='title' />
+                        <div className='openText'>
+                            <div>
+                                <b>ימי א׳ -ה׳-</b>
+                                בוקר- 06:30-08:00
+                                צהריים- 11:30-14:00
+                                ערב- 18:00-20:00                            
+                            </div>
+                            <div>
+                                <b>ימי שישי-</b>
+                                בוקר- 06:30-08:00
+                                צהריים- 11:30-13:30
+                                ערב -משתנה בהתאם
+                                לכניסת השבת.
+                            </div>
+                            <div>
+                                <b>ימי שבת-</b>
+                                בוקר- 06:30-08:00
+                                צהריים- 11:30-14:30
+                                ערב- משתנה בהתאם לזמן
+                                יציאת שבת.
+                            </div>
+                            <div>
+                            מידי שבוע מופץ ויסות חדרי אוכל
+                            קרייתי במסרתו נקבעות שעות לכל
+                            בה”ד, יש להקפיד להגיע בשעות
+                            שהוקצו לבה”ד.
+                            </div>
+                        </div>
+                        <div>
+                            <img src={MiddleMeal} alt="middle" className='title' />
+                            <div className='middleText'>
+                            בקריית ההדרכה ניתן להזמין ולמשוך
+                            ארוחות ביניים מעמדת הפריסה
+                            שהוגדרה בצמוד לרמפה של המטבח.
+                                <p> איך מזמינים ארוחת ביניים?
+                                פנו לתכנון הצבאי של הבה״ד שלכם -
+                                דרישה למפקדת הסעדה - הזנה
+                                במערכת ניהול העיר - ובאמצעות
+                                מספר המפגש שלכם תוכלו למשוך
+                                את הפריסה.</p>
+                            </div>
+                        </div>
+                        <div>
+                            <img src={Kashrut} alt="kasher" className='title' />
+                            <div className='kosherText'>
+                                <p>בשר גולמי וטחון חלק מהרבנות.
+                                מוצרי עוף רבנות מהדרין.</p> 
+                                <p>ירקות ופירות שמיטא לחומרה. <img src={Kosher} className='kosherPic' /></p>
+                                <p>ירק עלי בפיקוח חרקים למהדרין.
+                                חומרי גלם ומוצרי חלב בד”צ העדה
+                                החרדית בחדר אוכל A. </p>
+                                <p>
+                                מס טלפון אחראי כשרות ודת-
+                                רס”ב נאור בוכובזה - <a href="tel: +0526374225">0526374225</a>
+                                </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
